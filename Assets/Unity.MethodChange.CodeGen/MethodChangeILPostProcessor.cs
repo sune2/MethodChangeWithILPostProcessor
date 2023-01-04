a160e2d9aa54d88a2e4f44c902d0c377fe871ef8
Kazunori Tamura <tamura_kazunori@cyberagent.co.jp>
2023-01-04 14:35:56 +0900

    書き換え対象がinstanceメソッドの場合への対応とLdarg_Sの利用

--- Assets/Unity.MethodChange.CodeGen/MethodChangeILPostProcessor.cs
+++ Assets/Unity.MethodChange.CodeGen/MethodChangeILPostProcessor.cs
@@ -54,18 +54,28 @@ public class MethodChangeILPostProcessor : ILPostProcessor
         // メソッドの実装のILの先頭を取得
         var first = processor.Body.Instructions[0];
 
-        // 引数ロードのILコードの追加（この実装では、引数の数が4つまでに対応している）
+        // 引数ロードのILコードの追加
         for (var i = 0; i < method.GetParameters().Length; i++)
         {
-            var code = i switch
+            var argNum = i;
+            if (method.IsStatic == false)
+            {
+                // instanceメソッドではarg_0にthisが入るのでその分をずらす
+                argNum++;
+            }
+
+            var code = argNum switch
             {
                 0 => OpCodes.Ldarg_0,
                 1 => OpCodes.Ldarg_1,
                 2 => OpCodes.Ldarg_2,
                 3 => OpCodes.Ldarg_3,
-                _ => OpCodes.Nop // 未対応
+                _ => OpCodes.Ldarg_S
             };
-            processor.InsertBefore(first, processor.Create(code));
+            var instruction = code == OpCodes.Ldarg_S
+                ? processor.Create(code, methodDefinition.Parameters[i].Resolve())
+                : processor.Create(code);
+            processor.InsertBefore(first, instruction);
         }
 
         // メソッド呼び出しに必要なMethodReferenceはModuleDefinitionのImportRefenreceメソッドで取得可能
